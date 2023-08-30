c4d902d4e3a5e00b963337fcafeb568b1a51d63b
Matthew Harrilal <matthewharrilal@gmail.com>
2023-08-30 09:26:02 -0700

    Testing unabstracted version

--- print.c
+++ print.c
@@ -3,6 +3,88 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+void formatSpecifier(va_list args, const char *format, int *counter)
+{
+	char c;
+	char *nullStr;
+	
+	switch (*format)
+	{
+		case 'c':
+		    	c = va_arg(args, int);
+    			putchar(c);	
+    			*counter++;
+			break;
+		case 's':
+			s = va_arg(args, char *);
+			if (s == NULL)
+			{
+				nullStr = "(null)";
+				while (*nullStr != '\0')
+				{
+					putchar(*nullStr);
+					nullStr++;
+					*counter++;
+				}
+			} else
+			{
+				/* Loop through entire string  */
+				while (*s != '\0')
+				{
+					putchar(*s);
+					s++;
+					*counter++;
+				}
+			}
+			break;
+		case '%':
+			putchar('%');
+			*counter++;
+			break;
+		case 'd':
+			d = va_arg(args, int);
+
+			 if (d == 0)
+			 {
+				 putchar('0');
+				 counter++;
+			 }
+			 else if (d < 0)
+			 {
+				 putchar('-');
+				 counter++;
+				 d = -d;
+			 }
+
+			 /* Convert each digit to character and print */
+			 numDigits = 0;
+			 temp = d;
+			 while (temp > 0)
+			 {
+				 temp /= 10;
+				 numDigits++;
+			 }
+
+			 divisor = 1;
+			 for (i = 1; i < numDigits; i++)
+			 {
+				 divisor *= 10;
+			 }
+
+			 while (divisor > 0)
+			 {
+				 currentDigit = d / divisor;
+				 putchar('0' + currentDigit); /* Convert to character before printing */
+				 counter++;
+				 d %= divisor;
+				 divisor /= 10;
+			 }
+ 			break;
+		default:
+			break;			
+	}
+}
+
 int _printf(const char *format, ...)
 {
         char c;
@@ -89,6 +171,7 @@ int _printf(const char *format, ...)
 				 {
 					 temp /= 10;
 					 numDigits++;
+					 printf("Temp: %d\n", temp);
 				 }
 
 				 divisor = 1;
@@ -97,13 +180,16 @@ int _printf(const char *format, ...)
 					 divisor *= 10;
 				 }
 
+				 printf("Divisor before decrementing %d\n", divisor);
 				 while (divisor > 0)
 				 {
 					 currentDigit = d / divisor;
+					 printf("Current Digit: %d\n", currentDigit);
 					 putchar('0' + currentDigit); /* Convert to character before printing */
 					 counter++;
 					 d %= divisor;
 					 divisor /= 10;
+					 printf("Divisor after decrementing %d\n", divisor);
 				 }
 
 			} else
