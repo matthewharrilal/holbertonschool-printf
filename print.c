1a9f28014ecbe9bed2a87845e2584892ada60472
Matthew Harrilal <matthewharrilal@gmail.com>
2023-08-30 09:52:32 -0700

    Fixed comments

--- print.c
+++ print.c
@@ -36,7 +36,7 @@ void formatSpecifier(va_list args, const char *format, int *counter)
 				}
 			} else
 			{
-				/ Loop through entire string  /
+				/* Loop through entire string */
 				while (*s != '\0')
 				{
 					putchar(*s);
@@ -64,7 +64,7 @@ void formatSpecifier(va_list args, const char *format, int *counter)
 				 d = -d;
 			 }
 
-			 / Convert each digit to character and print /
+			 /* Convert each digit to character and print */
 			 numDigits = 0;
 			 temp = d;
 			 while (temp > 0)
@@ -82,7 +82,7 @@ void formatSpecifier(va_list args, const char *format, int *counter)
 			 while (divisor > 0)
 			 {
 				 currentDigit = d / divisor;
-				 putchar('0' + currentDigit); / Convert to character before printing /
+				 putchar('0' + currentDigit); /* Convert to character before printing */
 				 (*counter)++;
 				 d %= divisor;
 				 divisor /= 10;
